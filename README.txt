b5ec3bd3984694f47dcffd179e348062f77fcf02
Andrei <andreicomanescuonline@gmail.com>
2022-01-28 12:12:25 +0200

    First commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+Color Flipper
+
+Make a website that when a button is pressed,the background color will change and it
+will display the current background color above the button.There are also 2 modes: Simple,
+where it displays normal,hex,and rgba colours;Hex,where it displays only hex values.
+
+Tasks:
+-make the button not move when clicking ❌
+-add a hex and rgba colours to CSS_COLOR_NAMES
---
+++ app.js
@@ -0,0 +1,196 @@
+const CSS_COLOR_NAMES = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGrey",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkSlateGrey",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Grey",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSlateGrey",
+  "LightSteelBlue",
+  "LightYellow",
+  "Lime",
+  "LimeGreen",
+  "Linen",
+  "Magenta",
+  "Maroon",
+  "MediumAquaMarine",
+  "MediumBlue",
+  "MediumOrchid",
+  "MediumPurple",
+  "MediumSeaGreen",
+  "MediumSlateBlue",
+  "MediumSpringGreen",
+  "MediumTurquoise",
+  "MediumVioletRed",
+  "MidnightBlue",
+  "MintCream",
+  "MistyRose",
+  "Moccasin",
+  "NavajoWhite",
+  "Navy",
+  "OldLace",
+  "Olive",
+  "OliveDrab",
+  "Orange",
+  "OrangeRed",
+  "Orchid",
+  "PaleGoldenRod",
+  "PaleGreen",
+  "PaleTurquoise",
+  "PaleVioletRed",
+  "PapayaWhip",
+  "PeachPuff",
+  "Peru",
+  "Pink",
+  "Plum",
+  "PowderBlue",
+  "Purple",
+  "RebeccaPurple",
+  "Red",
+  "RosyBrown",
+  "RoyalBlue",
+  "SaddleBrown",
+  "Salmon",
+  "SandyBrown",
+  "SeaGreen",
+  "SeaShell",
+  "Sienna",
+  "Silver",
+  "SkyBlue",
+  "SlateBlue",
+  "SlateGray",
+  "SlateGrey",
+  "Snow",
+  "SpringGreen",
+  "SteelBlue",
+  "Tan",
+  "Teal",
+  "Thistle",
+  "Tomato",
+  "Turquoise",
+  "Violet",
+  "Wheat",
+  "White",
+  "WhiteSmoke",
+  "Yellow",
+  "YellowGreen",
+];
+
+// console.log(CSS_COLOR_NAMES.length);
+
+const colorText = document.querySelector("span"); //The span
+const clickmebutton = document.querySelector("button");
+console.log(colorText);
+
+function rgbRandom() {
+  let first_color = Math.floor(Math.random() * 148);
+  let second_color = Math.floor(Math.random() * 148);
+  let third_color = Math.floor(Math.random() * 148);
+
+  const rgbColor = `rgb(${first_color}, ${second_color}, ${third_color})`;
+
+  let background = document.querySelector(".maincontent");
+  background.style.backgroundColor = rgbColor;
+
+  colorText.style.color = rgbColor;
+  colorText.innerText = rgbColor;
+  clickmebutton.style.backgroundColor = rgbColor;
+  return rgbColor;
+}
+
+function colorRandom() {
+  let colorIndex = Math.floor(Math.random() * 148);
+  console.log(CSS_COLOR_NAMES[colorIndex]);
+
+  let background = document.querySelector(".maincontent");
+  background.style.backgroundColor = CSS_COLOR_NAMES[colorIndex];
+
+  colorText.style.color = CSS_COLOR_NAMES[colorIndex];
+  colorText.innerText = CSS_COLOR_NAMES[colorIndex];
+  clickmebutton.style.backgroundColor = CSS_COLOR_NAMES[colorIndex];
+  return CSS_COLOR_NAMES[colorIndex];
+}
+
+clickmebutton.addEventListener("click", function () {
+  const randomChoice = Math.floor(Math.random() * 2 + 1);
+  console.log(randomChoice);
+  // Random Color
+  if (randomChoice == 1) {
+    colorRandom();
+  } else if (randomChoice == 2) {
+    rgbRandom();
+  }
+});
